530aced8baf1c0d11794f127765a021ca9247eeb
WalukaUI <cwaluka@yahoo.com>
2021-05-16 00:25:54 -0700

    Passing All

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,19 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [dark,setDark]=useState(false)
 
+  function setColor(){
+   setDark(!dark)
+  }
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (dark ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={setColor}>Dark Mode</button>
       </header>
       <ShoppingList items={items} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+
+  function addItem() {
+    setInCart(!inCart)
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? "added" : "add"} onClick={addItem}>{inCart ? "In Cart" : "Add to Cart"}</button>
     </li>
   );
 }
+
+
+
 
 export default Item;
